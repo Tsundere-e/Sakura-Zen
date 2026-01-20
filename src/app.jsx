c821b648d76f0f3e509c993c1b3776b208418423
Ana Luisa <analuisa.r.dos.s@gmail.com>
2026-01-20 19:03:42 -0300

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo, useCallback, memo } from 'react';
+import React, { useState, useEffect, useMemo, useCallback, useRef, memo } from 'react';
 import { 
   Flower, 
   CheckCircle2, 
